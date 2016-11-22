e4ad3070c6e88352d0296b7fcaf80124d55105d1
Seungjae Moon <sjaymoon15@gmail.com>
2016-11-22 17:54:22 -0500

    test creating user

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-	.once('open', () => console.log('Good to go!'))
-	.on('error', (error) => {
-		console.warn('Warning', error);
+mongoose.Promise = global.Promise;
+
+before((done) => {
+	mongoose.connect('mongodb://localhost/users_test');
+	mongoose.connection
+		.once('open', () => {
+			console.log('mongo and mongoose connected')
+			done();
+		})
+		.on('error', (error) => {
+			console.warn('Warning', error);
+		});
+});
+
+//when this is done, (after it hits done();) then run the rest
+beforeEach((done) => {
+	mongoose.connection.collections.users.drop(() => {
+		//ready to run the next test!
+		done();
 	});
+});
