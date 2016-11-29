bb79ce2b3993eabf9eca230e53f712889bca348f
Seungjae Moon <sjaymoon15@gmail.com>
2016-11-29 16:52:07 -0500

    add helper function assertName for update test

--- test/update_test.js
+++ test/update_test.js
@@ -9,17 +9,24 @@ describe('Updating records', () => {
 		joe = new User({ name: 'Joe' });
 		joe.save()
 		.then(() => done());
-	})
+	});
 
-	it('instance type using set and save', (done) => {
-		joe.set({ name: 'Kay' });
-		joe.save()
+	function assertName(operation, done){
+		operation
 		.then(() => User.find({}))
 		.then((users) => {
 			assert(users.length === 1);
 			assert(users[0].name === 'Kay');
 			done();
 		})
+	}
+
+	it('instance type using set and save', (done) => {
+		joe.set({ name: 'Kay' });
+		assertName(joe.save(), done);
 	});
 
+	it('A model instance can update', (done) => {
+		assertName(joe.update({ name: 'Kay' }), done);
+	});
 });
