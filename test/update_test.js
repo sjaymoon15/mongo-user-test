f7f5201fb7927ec4b309bcb6f78b9ef5d58772f3
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-03 14:57:30 -0800

    modify user model

--- test/update_test.js
+++ test/update_test.js
@@ -6,7 +6,7 @@ describe('Updating records', () => {
 	let joe;
 
 	beforeEach((done) => {
-		joe = new User({ name: 'Joe' , postCount: 0 });
+		joe = new User({ name: 'Joe' , likes: 0 });
 		joe.save()
 		.then(() => done());
 	});
@@ -43,12 +43,12 @@ describe('Updating records', () => {
 		assertName( User.findByIdAndUpdate({ _id: joe._id}, { name: 'Kay'}), done);
 	});
 
-	xit('A user can have their postcount incremented by 1', (done) => {
+	it('A user can have their likes incremented by 1', (done) => {
 		//let's use update operator(modifier)
-		User.update({ name: 'Joe' }, { $inc: { postCount : 1} })
+		User.update({ name: 'Joe' }, { $inc: { likes : 1} })
 		.then(() => User.findOne({ name: 'Joe' }))
 		.then((user) => {
-			assert(user.postCount === 1);
+			assert(user.likes === 1);
 			done();
 		})
 	});
