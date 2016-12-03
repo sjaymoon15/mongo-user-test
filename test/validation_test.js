99d6835e635d39f7ccc90390ce4e2636979e8227
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-02 18:31:32 -0800

    disallow invalid records from being saved

--- test/validation_test.js
+++ test/validation_test.js
@@ -10,9 +10,19 @@ describe('Validating records', () => {
 	});
 
 	it('requires a user name longer than 2 characters', () => {
-		const user = new User( { name: 'Al' });
+		const user = new User({ name: 'Al' });
 		const validationResult = user.validateSync();
 		const { message } = validationResult.errors.name;
 		assert( message === 'Name should be at least 3 characters.');
+	});
+
+	it('disallows invalid records from being saved', (done) => {
+		const user = new User({ name: 'Al' });
+		user.save()
+		.catch((validationResult) => {
+			const { message } = validationResult.errors.name;
+			assert( message === 'Name should be at least 3 characters.');
+			done();
+		});
 	});
 });
