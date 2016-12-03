32d3fd2fadb1fa78cdfc6a5e1cf05b84074a2be0
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-02 19:34:01 -0800

    test removing subdocument

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -34,4 +34,36 @@ describe('Subdocuments', () => {
 			done();
 		})
 	});
+
+	it('can remove an exisiting subdocument', (done) => {
+		const joe = new User({
+			name: 'Joe',
+		 	posts: [{ title: 'PostTitle' }]
+		});
+
+		joe.save()
+		.then(() => User.findOne({ name: 'Joe'}))
+		.then((user) => {
+			user.posts[0].remove();
+			return user.save();
+		})
+		.then(() => User.findOne({ name: 'Joe' }))
+		.then((user) => {
+			assert(user.posts.length === 0);
+			done();
+		})
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
