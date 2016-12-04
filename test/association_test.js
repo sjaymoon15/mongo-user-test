6936321a04b9896bb27a59ba507d078d0d9b1636
Seungjae Moon <sjaymoon15@gmail.com>
2016-12-03 16:39:54 -0800

    test a full relation graph

--- test/association_test.js
+++ test/association_test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const User = require('../src/user');
-const Comment = require('../src/comment');
+const Comment = require('../src/comments');
 const BlogPost = require('../src/blogPost');
 
 describe('Associations', () => {
@@ -15,5 +16,39 @@ describe('Associations', () => {
 		blogPost.comments.push(comment);
 		//assign joe ref (not the entire joe this)
 		comment.user = joe;
+
+		Promise.all([joe.save(), blogPost.save(), comment.save()])
+		.then(() => done())
+	});
+
+	it('saves a relation between a user and a blogpost', (done) => {
+		User.findOne({ name: 'Joe' })
+		.populate('blogPosts')
+		.then((user) => {
+			assert(user.blogPosts[0].title === 'JS is good');
+			done();
+		});
+	});
+
+	it('saves a full relation graph', (done) => {
+		User.findOne( { name: 'Joe' })
+		.populate({
+			path: 'blogPosts',
+			populate: {
+				path: 'comments',
+				model: 'comment',
+				populate: {
+					path: 'user',
+					model: 'user'
+				}
+			}
+		})
+		.then((user) => {
+			assert(user.name === 'Joe');
+			assert(user.blogPosts[0].title === 'JS is good');
+			assert(user.blogPosts[0].comments[0].content === 'Congrats comment');
+			assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+			done();
+		});
 	});
 });
